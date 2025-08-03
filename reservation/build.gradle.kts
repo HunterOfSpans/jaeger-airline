b163c76e093c07ab4b95e4e2588d7f66778d55da
wlsdks <wlsdks12@naver.com>
2025-08-03 14:43:15 +0900

    :sparkles: flight, payment service added

---
+++ reservation/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.airline"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+dependencyManagement{
+	imports {
+		mavenBom ("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:2.6.0")
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation ("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation ("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation ("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+	implementation ("org.springframework.boot:spring-boot-starter-web")
+	compileOnly ("org.projectlombok:lombok")
+//    runtimeOnly ("io.micrometer:micrometer-registry-otl"(
+	annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor ("org.projectlombok:lombok")
+	testImplementation ("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+	implementation ("io.micrometer:micrometer-tracing-bridge-otel")
+	implementation ("io.github.openfeign:feign-micrometer:13.6")
+	implementation ("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter")
+
+
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
